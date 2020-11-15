9713bf836bc773829c81c1209af5db3f3e860995
bbbyung2 <bhko0524@naver.com>
2020-11-16 00:52:32 +0900

    JS로 TS처럼 개발하는 방법

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,5 +1,20 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// sum(10, 20);
+// let result = sum(10, 20);
+// result.toLocaleString();
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ */
+
 function sum(a, b) {
     return a + b;
 }
-
-sum(10, 20);
+let result = sum(10, '20');
